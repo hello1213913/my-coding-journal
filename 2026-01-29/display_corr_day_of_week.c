2c9d07ce7924e86c897f80837c7c5311e523c17d
Labi <labirana53@gmail.com>
2026-01-29 17:22:31 +0545

    2026-01-29: added display_corr_day_of_week.c and log.md

---
+++ 2026-01-29/display_corr_day_of_week.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int num;
+
+    printf("Enter the number from 1 to 7 to know which number denotes what\n");
+    scanf("%d", &num);
+
+    if(num>1 && num <=7){
+        switch(num){
+            case 1: printf("\nsunday");
+                break;
+            case 2: printf("\nMonday");
+                break;
+            case 3: printf("\nTuesday");
+                break;
+            case 4: printf("\nWednesday");
+                break;
+            case 5: printf("\nThursday");
+                break;
+            case 6: printf("\nFriday");
+                break;
+            case 7: printf("\nSaturday");
+
+
+        }
+
+    }
+    else
+        printf("\nInvalid try again");
+
+    printf("\nThank you..\nHave a great day");
+    return 0;
+}
